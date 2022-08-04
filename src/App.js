01589f4eeb12baba4ed0c9b9e5ac65e977aa9553
SergeyBerez <sergeyberezovsky1981@gmail.com>
2022-08-04 22:30:10 +0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main-section">
+      <header className="header">
+        <div className="header-bg">
+          <div cal></div>
+          <h1 className="text-3xl font-bold underline">
+            Профессия Frontend — разработчик
+          </h1>
+
+          <div className="circles-block">
+            <div className="circles"></div>
+            <div className="circles"></div>
+            <div className="circles"></div>
+          </div>
+        </div>
       </header>
+      <div className="container mx-auto"></div>
     </div>
   );
 }
-
 export default App;
