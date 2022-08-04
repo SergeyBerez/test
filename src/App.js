581a3b96227a380823a7454f906ad53a67c0ec7f
SergeyBerez <sergeyberezovsky1981@gmail.com>
2022-08-04 22:38:27 +0300

    add block

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
     <div className="main-section">
       <header className="header">
         <div className="header-bg">
-          <div cal></div>
-          <h1 className="text-3xl font-bold underline">
-            Профессия Frontend — разработчик
-          </h1>
+          <div className="header-title-block">
+            <h1 className="text-3xl font-bold underline">
+              Профессия Frontend — разработчик
+            </h1>
+          </div>
 
           <div className="circles-block">
             <div className="circles"></div>
