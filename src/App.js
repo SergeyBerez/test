d20df236527651505ed2c77dd72d8cac022ecb41
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2022-08-07 20:23:02 +0300

    add section main

--- src/App.js
+++ src/App.js
@@ -1,42 +1,76 @@
-import "./App.css";
-import Dots from "./components/Dots";
-import ButtonTooltip from "./components/ButtonTooltip";
+import './App.css';
+import Dots from './components/Dots';
+import ButtonTooltip from './components/ButtonTooltip';
 
 function App() {
   return (
     <>
       <header className="header">
-        <div className="header-bg">
-          <div className="circles-block">
-            <div className="circles"></div>
-            <div className="circles"></div>
-            <div className="circles"></div>
+        <div className="container mx-auto">
+          <div className="header-bg">
+            <div className="header-title-block">
+              <h1 className="h1-title">
+                <p>Профессия</p> Frontend — разработчик
+              </h1>
+              <p className="header-subtitle">
+                Стань с нуля продвинутым Junior разработчиком за 7 месяцев с знанием технологий
+                больше, чем у других кандидатов
+              </p>
+              <p className="header-subtitle-description">
+                Начиная с основ изучишь JavaScript, разберешься с самыми популярными технологиями,
+                научишься писать Frontend на фреймворке и связывать его с Backend. Навыки после
+                курса оцениваются от 100 т.р.
+              </p>
+            </div>
+            <div className="section-button-circles">
+              <div className="circles-block">
+                <div className="circles">
+                  <button className="button-header">задать вопрос об обучении</button>
+                </div>
+                <div className="circles"></div>
+                <div className="circles"></div>
+              </div>
+            </div>
+            {/* </div> */}
           </div>
-          <div className="header-title-block">
-            <h1 className="h1-title">
-              <p>Профессия</p> Frontend — разработчик
-            </h1>
-            <p className="header-subtitle">
-              Стань с нуля продвинутым Junior разработчиком за 7 месяцев с
-              знанием технологий больше, чем у других кандидатов
-            </p>
-            <p className="header-subtitle-description">
-              Начиная с основ изучишь JavaScript, разберешься с самыми
-              популярными технологиями, научишься писать Frontend на фреймворке
-              и связывать его с Backend. Навыки после курса оцениваются от 100
-              т.р.
-            </p>
-            <button className="button-header">задать вопрос об обучении</button>
-          </div>
+          <ButtonTooltip></ButtonTooltip>
+          <Dots cls="up"></Dots>
+          <Dots cls="down"></Dots>
 
-       
+          <div className="section-numbers">
+            <div className="number-block">
+              <div className="number">293</div>
+              <p className="number-text">
+                <p>HD видеоурока</p> длительностью 3 - 15 минут
+              </p>
+            </div>
+            <div className="number-block">
+              <div className="number">6</div>
+              <p className="number-text">
+                {' '}
+                Мероприятий на курсе (Хакатоны, мастер классы, разборы и т.д.)
+              </p>
+            </div>
+            <div className="number-block">
+              <span className="number">193</span>
+              <p className="number-text">заданий и тестов для закрепления</p>
+            </div>
+          </div>
         </div>
-        <ButtonTooltip></ButtonTooltip>
-        <Dots cls="up"></Dots>
-        <Dots cls="down"></Dots>
       </header>
 
-      <div className="container mx-auto"></div>
+      <main>
+        <div className="container mx-auto">
+          <div className="section-description">
+            <h2 className="description-title">Как именно ты станешь разработчиком</h2>
+            <p className="description-subtitle">
+              На курсе ты не просто изучаешь теоретический материал. в процессе изучения ты
+              повторяете за действующим разработчиком, после чего{' '}
+              <b>закрепляешь знания на заданиях и проектах.</b>
+            </p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/ButtonTooltip.js
+++ src/components/ButtonTooltip.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import exelIcon from "../../src/Vector.svg";
-import layer from "../../src/Property 1=Default.png";
+import React, { useState } from 'react';
+import exelIcon from '../../src/Vector.svg';
+import layer from '../../src/Property 1=Default.png';
 function ButtonTooltip() {
   const [isShown, setIsShown] = useState(false);
   const [cls, SetCls] = useState(true);
@@ -14,19 +14,17 @@ function ButtonTooltip() {
         onClick={showTooltipBlock}
         className="button-exel"
         onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      >
-        <img src={exelIcon} />
+        onMouseLeave={() => setIsShown(false)}>
+        <img src={exelIcon} alt="button" />
       </button>
-      <div className={cls ? "exel-animation" : "exel-animation moveleft"}>
-        <img src={layer}></img>
+      <div className={cls ? 'exel-animation' : 'exel-animation moveleft'}>
+        <img src={layer} alt="users"></img>
       </div>
       {isShown && (
         <div className="tooltip-box">
           <p className="tooltip-box-text">
-            Обучение для тех, кто готов учиться 10 часов в неделю, чтобы
-            получить результат. Мы даем необходимые навыки на продвинутом
-            уровне, убирая все лишнее
+            Обучение для тех, кто готов учиться 10 часов в неделю, чтобы получить результат. Мы даем
+            необходимые навыки на продвинутом уровне, убирая все лишнее
           </p>
         </div>
       )}
